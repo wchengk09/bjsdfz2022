9fdd06daa30ae9d76bfdde68e9111723ebdac613
bjsdfz17ban <wchengk09@163.com>
2023-06-01 03:00:06 +0800

    WCK changed some code

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ var curImg = Imgs - 1;
     getObj('img').src = '//kevinwu521.gitee.io/res/fz/' + curImg + '.jpg';
     setTimeout(switchImage,4000);
 })();
+
+function setWidth(){
+    const wid = window.innerWidth;
+    const cnt = Math.floor(wid / 250);
+    const every = wid / cnt;
+    console.log(every);
+    const doms = document.querySelectorAll('.index-block-out');
+    console.log(doms);
+    for (var i = 0;i < doms.length;i ++){
+        doms[i].style.width = every + '';
+        console.log(doms[i].style);
+    }
+
+}
+window.addEventListener('load',setWidth);
+window.addEventListener('resize',setWidth);
