b997bdcf652ac64f57e2c38d7b871bf1ea03d67b
bjsdfz17ban <wchengk09@163.com>
2023-06-10 15:02:11 +0800

    update

---
+++ backEnd17/common.js
@@ -0,0 +1,24 @@
+exports.querySync = function(sql){
+    return new Promise(function(resolve,reject){
+        conn.query(sql,function(err,res){
+            if (err)reject(err);
+            resolve(res);
+        })
+    })
+}
+
+exports.getUserId = function(sessid){
+    return new Promise(function(resolve,reject){
+        conn.query('SELECT * FROM sess_users WHERE sessid=',function(err,res){
+            if (err)reject(err);
+            resolve(res[0].id);
+        })
+    })
+}
+
+exports.set200 = function(res){
+    res.writeHead(200,{
+        "Content-Type":"text/html;charset=utf-8",
+        "Access-Control-Allow-Origin":"*"
+    });
+}
---
+++ backEnd17/entry.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql');
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+global.conn = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	database: 'zizhihui'
+});
+conn.on('error',err=>{
+	console.log('Re-connecting lost connection: ');
+	global.conn = mysql.createConnection({
+		host: '127.0.0.1',
+		user: 'root',
+		database: 'zizhihui'
+	})
+});
+process.on('uncaughtException',function(err){console.log(err);})
+
+http.createServer(function(req,res){
+	var url = '.' + req.url;
+	if (url.indexOf('..') !== -1){
+		res.writeHead(400);
+		res.end('Invalid URL!');
+		return;
+	}
+	var post = '';
+    req.on('data',function(chunk){
+        post += chunk;
+    });
+
+    req.on('end',function(){
+		post = querystring.parse(post);
+		var modu = require(url);
+		modu.main(req,res,post);
+	});
+}).listen(17666);
+
+console.log('Server started.');
+
