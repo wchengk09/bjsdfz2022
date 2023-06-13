8b6df1a433f9251948de770c324cbe33ea5d9d52
bjsdfz17ban <wchengk09@163.com>
2023-06-13 15:48:54 +0800

    WCK changed some code

---
+++ login/index.js
@@ -0,0 +1,20 @@
+const { query } = require("../backEnd/MySQLCURD");
+
+function login(){
+    var username = getObj('username').value;
+    var password = getObj('password').value;
+    var ajax = new XMLHttpRequest();
+    ajax.onload = function(){
+        alert(ajax.responseText);
+        if (ajax.responseText === '登录成功！')
+            location.href = '/';
+    }
+    ajax.open('POST',getIP(18667));
+    ajax.send(queryString({
+        mode: 'user',
+        type: 'login',
+        username: username,
+        password: password,
+        sessionid: sessID()
+    }));
+}
