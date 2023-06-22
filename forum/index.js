9c9817024c4528bcb8baf3f0474f1fbb235fec55
bjsdfz17ban <wchengk09@163.com>
2023-06-23 03:00:09 +0800

    WCK changed some code

--- forum/index.js
+++ forum/index.js
@@ -1,12 +1,24 @@
 window.addEventListener('load',function(){
+    const IP = getIP(17668);
     var ajax = new XMLHttpRequest();
     ajax.onload = function(){
+        getObj('forumList').innerHTML = '';
         var spl1 = ajax.responseText.split('&');
-        for (var i = 0;i < spl1.length;i ++){
+        console.log(spl1);
+        if (!spl1[0])return;
+        for (var i = spl1.length - 1;i >= 0;i --){
             ajax.onload = function(){
                 var spl2 = ajax.responseText.split('&');
-                
+                getObj('forumList').innerHTML += 
+                '<div class="forum-container">'
+                + '<h2 class="blue">' + decodeURIComponent(spl2[0]) + '</h2>'
+                + '<p>' + decodeURIComponent(spl2[1])
+                + '</div>';
             }
+            ajax.open('POST',IP + '/forum/content');
+            ajax.send('id=' + spl1[i]);
         }
     }
+    ajax.open('POST',IP + '/forum/childs');
+    ajax.send('fa=0');
 })
